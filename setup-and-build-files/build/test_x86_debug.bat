e201ba7da5e27392f6140712bb57550ec38dfc50
doggogit <andrejjosif97@gmail.com>
2022-11-06 23:35:51 +0100

    these are not needed now

--- setup-and-build-files/build/test_x86_debug.bat
+++ setup-and-build-files/build/test_x86_debug.bat
@@ -4,8 +4,6 @@ title FNF: Forever Engine Feather - Running Game (DEBUG MODE)
 cd ..
 cd ..
 echo BUILDING...
-echo IF IT CRASHES AFTER THE TITLESCREEN OR WHEN GOING TO PLAYSTATE
-echo TRY BINDING THE RESET KEYS TO ANYTHING ON A RELEASE BUILD
 haxelib run lime test windows -debug -D HXCPP_M32
 echo. 
 echo DONE
