d22c5e121867f164e901dce235217887ca386ed4
Gabriela <45212377+BeastlyGhost@users.noreply.github.com>
2022-11-12 14:52:00 -0400

    Update schoolEvil.hx

--- assets/stages/schoolEvil/schoolEvil.hx
+++ assets/stages/schoolEvil/schoolEvil.hx
@@ -1,6 +1,6 @@
 function dadPosition(boyfriend:Character, gf:Character, dad:Character)
 {
-    var evilTrail:FlxTrail = new FlxTrail(dad, null, 4, 24, 0.3, 0.069);
+	var evilTrail:FlxTrail = new FlxTrail(dad, null, 4, 24, 0.3, 0.069);
 	evilTrail.changeValuesEnabled(false, false, false, false);
 	add(evilTrail);
 }
